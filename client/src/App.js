fc6890d19d543c75e142de87894f91bc7fcd11d7
naman159 <namanranawat159@gmail.com>
2020-04-16 15:32:24 +0530

    Complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Login from './components/Login/Login';
+
 import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Route exact path = '/' component={Login} />
-      <Route exact path = '/chat' component={Chat} />
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
     </Router>
   );
 }
